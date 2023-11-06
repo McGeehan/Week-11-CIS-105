f25acc2d7f3a4f85cd2cf5c7bbc2c4959402a033
MkGee <matthewrmcgeehan2@gmail.com>
2023-11-06 14:13:54 -0500

    Update codebug.py

--- codebug.py
+++ codebug.py
@@ -11,10 +11,13 @@ print("The area of the circle is:", circle_area)
 
 
 '''
-    The first bug is on the line return areas. The variable areas is not defined in the function. The correct variable name is area.
+    The first bug is on the line return areas. The variable areas is not defined in the function. 
+    The correct variable name is area.
 
-    The second bug is on the line radius = int(radius_input). If the user enters a non-integer value, this line will raise a ValueError. To fix this, you should use a try-except block to handle the error.
+    The second bug is on the line radius = int(radius_input). If the user enters a non-integer value, 
+    this line will raise a ValueError. To fix this, you should use a try-except block to handle the error.
 
-    The third bug is on the line print("The area of the circle is:", circle_area). If the calculate_area function raises an error (for example, because of the first bug), this line will not be executed, and the user will not see any output. To fix this, you should move the print statement inside a try-except block, so that it is executed even if there is an error.
+    The third bug is on the line print("The area of the circle is:", circle_area). 
+    If the calculate_area function raises an error (for example, because of the first bug), this line will not be executed, and the user will not see any output. To fix this, you should move the print statement inside a try-except block, so that it is executed even if there is an error.
 
     '''
